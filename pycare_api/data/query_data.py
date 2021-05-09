4c97c0558c534f2a5d4b21890fa706003922bc4b
PrinceSanjivy <sanjivy.android@gmail.com>
2021-05-09 19:12:07 +0530

    add mongodb

--- pycare_api/data/query_data.py
+++ pycare_api/data/query_data.py
@@ -1,1 +1,11 @@
-# get data from mongodb
+import pymongo
+
+url = 'mongodb+srv://princesanjivy:dnoLwh2MYLHwQS2W@snow-princebot.lgc4v.mongodb.net/test_pycare_api?retryWrites=true&w=majority'
+client = pymongo.MongoClient(url)
+
+db = client["test_pycare_api"]
+
+collection = db.get_collection("listHospitals")
+
+for doc in collection.find():
+    print(doc)
