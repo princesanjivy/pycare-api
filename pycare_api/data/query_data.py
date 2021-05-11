54153451e72cbaa9daa5fdd91a1a35f861d72cff
aswini17 <aaaaashwini12345@gmail.com>
2021-05-11 17:31:27 +0530

    dashboard page

--- pycare_api/data/query_data.py
+++ pycare_api/data/query_data.py
@@ -2,8 +2,12 @@ import pymongo
 import os
 from typing import Optional
 import scrape_data as data
+import home_data as dashboard
+import json
+from pymongo import MongoClient
 
 url = 'mongodb+srv://{}:{}@pycare-api.xbmlx.mongodb.net/covid19Report?retryWrites=true&w=majority'
+
 client = pymongo.MongoClient(url.format(
     os.getenv("username"), os.getenv("password")))
 
@@ -15,7 +19,6 @@ def getData(collectionName: str,
     if fields != None:
         showOnly = dict(zip(fields, [True]*len(fields)))
         showOnly["_id"] = False
-
         return db.get_collection(collectionName).find({}, showOnly)
     else:
         print("all")
@@ -39,3 +42,10 @@ def updateData():
     except:
         return "failed to update data"
 
+def updatestatusData():
+    try:
+        
+        Collection.update_one({},{"$set":{"total":a["total"],"cured":a["cured"],"active":a["active"],"death":a["death"]}})
+        return "successfully updated"
+    except:
+        return "failed to update data"
--- pycare_api/main.py
+++ pycare_api/main.py
@@ -1,70 +1,62 @@
-from fastapi import FastAPI
+import fastapi
+import scrape_data as data
 from typing import Optional
-from data import query_data as db
+import json
+from pymongo import MongoClient
 
-app = FastAPI(title="PyCare", description="API for pycare", version="1.0.0")
+app = fastapi.FastAPI(title="PyCare", description="API for pycare", version="1.0.0")
 
+@app.get("/bedAvailability",
+         responses={
+             200: {
+                 "description": '''Lists the number of availabilities of beds.\n
+                 Has optional query param "bedType" to get specific type of bed availability''',
+                 "content": {
+                     "application/json": {
+                         "example": [{
+                             "hospitalName": "PIMS",
+                             "lastUpdateOn":"09-05-2021 08:52:50",
+                             "isolationBeds": {
+                                 "alloted": "80",
+                                 "vacant": "3"
+                             },
+                             "oxygenBeds": {
+                                 "alloted": "5",
+                                 "vacant": "100"
+                             },
+                             "ventilatorBeds": {
+                                 "alloted": "100",
+                                 "vacant": "0"
+                             }
+                         }]
+                     }
+                 }
+             }
+         })
 
-# @app.get("/listHospitals",
-#          responses={
-#              200: {
-#                  "description": "List of hospitals in Puducherry",
-#                  "content": {
-#                      "application/json": {
-#                          "example": ["PIMS", "JIPMER"]
-#                      }
-#                  }
-#              }
-#          })
-# def listHospitals():
-#     return [i.hospitalName for i in data.availability]
+def bedAvailability(bedType: Optional[str] = None):
+    if bedType == None:
+        return data.availability
+    else:
+        return [{"hospitalName": i.hospitalName, bedType: getattr(i, bedType)} for i in data.availability]
 
+@app.get("/status",
+         responses={
+             200: {
+                 "description": "Covid status in Pondicherry",
+                 "content": {
+                     "application/json": {
+                         "example": [{
+                             "total": "70076",
+                             "cured":"55552",
+                              "active":"13585",
+                               "death":"939"
+                             }]
+                     }
+                 }
+             }
+         })
 
-# @app.get("/bedAvailability",
-#          responses={
-#              200: {
-#                  "description": '''Lists the number of availabilities of beds.\n
-#                  Has optional query param "bedType" to get specific type of bed availability''',
-#                  "content": {
-#                      "application/json": {
-#                          "example": [{
-#                              "hospitalName": "PIMS",
-#                              "isolationBeds": {
-#                                  "alloted": "80",
-#                                  "vacant": "5"
-#                              },
-#                              "oxygenBeds": {
-#                                  "alloted": "5",
-#                                  "vacant": "100"
-#                              },
-#                              "ventilatorBeds": {
-#                                  "alloted": "100",
-#                                  "vacant": "0"
-#                              }
-#                          }]
-#                      }
-#                  }
-#              }
-#          })
-# def bedAvailability(bedType: Optional[str] = None):
-#     if bedType == None:
-#         return data.availability
-#     else:
-#         return [{"hospitalName": i.hospitalName, bedType: getattr(i, bedType)} for i in data.availability]
-
-
-# example for status
-# make one for hospitalDetails
-
-@app.get("/status")
 def status():
-    return db.getData("status")[0]
-
-
-@app.get("/updateData")
-def updateData(updateOnly: Optional[str] = None):
-    # when query param is not passed in request header scrap and update both the collections
-    # else update only the collection which is requested
-
-    # write if part here
-    return {"updation": db.updateData()}
+    a=data.status()
+    return a
