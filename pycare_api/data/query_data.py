4421ec4063c9aeaab413f3d5bf3a2a876ad7650d
aswini17 <42395613+aswini17@users.noreply.github.com>
2021-05-16 00:27:41 +0530

    Update query_data.py

--- pycare_api/data/query_data.py
+++ pycare_api/data/query_data.py
@@ -1,13 +1,15 @@
 import pymongo
 import os
+from pycare_api.data import scrape_data as sdata
 from typing import Optional
 from pymongo import MongoClient
 
-url = "mongodb+srv://backend:sYPjEGvJzwPqFub3@pycare-api.xbmlx.mongodb.net/covid19Report?retryWrites=true&w=majority"
+db_username=os.environ.get('db_username')
+db_pass=os.environ.get('db_pass')
+url = "mongodb+srv://{}:{}@pycare-api.xbmlx.mongodb.net/covid19Report?retryWrites=true&w=majority".format(db_username,db_pass)
 client = pymongo.MongoClient(url.format(
     os.getenv("username"), os.getenv("password")))
 db = client["covid19Report"]
-
 
 def getData(collectionName: str, fields: Optional[list] = None):
     if fields != None:
@@ -17,6 +19,31 @@ def getData(collectionName: str, fields: Optional[list] = None):
         return db.get_collection(collectionName).find({}, showOnly)
     else:
         return db.get_collection(collectionName).find({}, {"_id": False})
+
+def updateHospitalDetailsData():
+    collection = db["hospitalDetails"]
+    try:
+        for i in sdata.hospitalDetails():
+            collection.update_many({"hospitalName": i.hospitalName},
+                                   {"$set": {"isolationBeds.alloted": i.isolationBeds['alloted'],
+                                             "isolationBeds.vacant": i.isolationBeds['vacant'],
+                                             "oxygenBeds.alloted": i.oxygenBeds['alloted'],
+                                             "oxygenBeds.vacant": i.oxygenBeds['vacant'],
+                                             "ventilatorBeds.alloted": i.ventilatorBeds['alloted'],
+                                             "ventilatorBeds.vacant": i.ventilatorBeds['vacant']}})
+        return "successfully updated hospitalDetails collection"
+    except Exception as err:
+        return str(err)+"failed to update data hospitalDetails collection"
+
+def updateStatusData():
+    collection = db["status"]
+    a = sdata.status()[0]
+    try:
+        collection.update_one({}, {"$set": {
+                              "total": a["total"], "cured": a["cured"], "active": a["active"], "death": a["death"]}})
+        return "successfully updated status collection"
+    except Exception as err:
+        return str(err)+"failed to update data status collection"
 
 
 
